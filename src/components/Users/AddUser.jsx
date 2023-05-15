34fea2e444f81b45d3cefae13c42b6c17e619ce5
Karan <karansethia24@gmail.com>
2023-05-15 09:09:47 +0530

    User form component

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const AddUser = props => {
+
+    const addUserHandler = event => {
+        event.preventDefault();
+    }
+
   return (
     <div>
         <h2>Age In Minutes Calculator</h2>
-        <form action="">
+        <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
             <input type="text" id="username" />
+            <label htmlFor="age">Age (in years)</label>
+            <input type="number" id="age" />
+            <button type="submit">Calculate</button>
         </form>
     </div>
   )
