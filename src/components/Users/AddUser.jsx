f563e243cf66f6f6399fb35acfc4d3ae077bd834
Karan <karansethia24@gmail.com>
2023-05-15 09:04:07 +0530

    initializing repo

---
+++ src/components/Users/AddUser.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddUser = props => {
+  return (
+    <div>
+        <h2>Age In Minutes Calculator</h2>
+        <form action="">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+        </form>
+    </div>
+  )
+}
+
+export default AddUser;
