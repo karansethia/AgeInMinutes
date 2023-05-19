e8242028f434d58800fdbd478f451efcaf60e8d7
Karan <karansethia24@gmail.com>
2023-05-19 11:37:56 +0530

    Completed Error handler

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -4,17 +4,30 @@ import logo from './logo.png'
 
 import Card from '../UI/Card'
 import Button from '../UI/Button'
+import ErrorModal from '../UI/ErrorModal'
 
 const AddUser = props => {
 
     const [name,setName] = useState('');
     const [age, setAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = event => {
         event.preventDefault();
-        if(name.trim().length == 0 || age.trim().length === 0){
+        if (name.trim().length === 0 || age.trim().length === 0) {
+            setError({
+                title: 'Invalid Name and age',
+                desc: 'Please enter a valid name and age'
+            });
             return;
-        }
+          }
+          if (+age < 1) {
+            setError({
+                title: 'Invalid Age',
+                desc: 'Please enter a valid age ,Age should be greater than 0'
+            });
+            return;
+          }
         props.onAddUser(name,(age*525600));
         setAge('');
         setName('');
@@ -27,8 +40,13 @@ const AddUser = props => {
     const setAgeHandler = event => {
         setAge(event.target.value);
     }
+    const errorHandler = () => {
+        setError(null);
+    }
 
   return (
+    <div>
+    {error && <ErrorModal title={error.title} desc={error.desc} onHandleError={errorHandler}/>}
     <Card className={styles.input}>
         <img src={logo} alt="" />
         <form onSubmit={addUserHandler}>
@@ -39,6 +57,7 @@ const AddUser = props => {
             <Button type="submit">Calculate</Button>
         </form>
     </Card>
+    </div>
   )
 }
 
